e298a30319581f139a800c8ad6cabf3d26b4cde9
SANJAY11SILU <beherasanjaykumar2@gmail.com>
2021-12-08 20:41:31 +0530

    initial commit

---
+++ src/app/commons/services/github/github.service.spec.ts
@@ -0,0 +1,19 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { GithubService } from './github.service';
+
+describe('GithubService', () => {
+  let service: GithubService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
+    service = TestBed.inject(GithubService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
